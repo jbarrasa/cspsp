4241aace7dcf657e14c66216ef4032111a10863b
J Barrasa <jbarrasa@Js-MacBook-Pro.local>
2017-01-30 02:51:44 -0500

    new non cypher version

--- src/main/java/cspsp/merge/MergeRelationship.java
+++ src/main/java/cspsp/merge/MergeRelationship.java
@@ -25,15 +25,43 @@ public class MergeRelationship {
 
     @Procedure( name = "cspsp.merge.relationship", mode = Mode.WRITE)
     public Stream<RelationshipResult> relationship(@Name("from") Node from,
-                                                   @Name("relType") String relType, @Name("props") String props,
+                                                   @Name("relType") String relType, @Name("props") Map<String, Object> props,
                                                    @Name("to") Node to) {
 
-        String cypher = " MATCH (from), (to)" +
-                        " WHERE ID(from) = " + from.getId() + " AND ID(to)= " + to.getId() +
-                        " MERGE (from)-[r:" + relType +  " " + props + " ]-(to) " +
-                        " RETURN r ";
-        Result result = db.execute(cypher);
-        return Stream.of(new RelationshipResult((Relationship) result.next().get("r")));
+        //First we try ro find the rel
+        Iterable<Relationship> relationships = from.getRelationships(RelationshipType.withName(relType), Direction.OUTGOING);
+        for (Relationship r:relationships){
+            if(matchesProperties(r.getAllProperties(), props)){
+                return Stream.of(new RelationshipResult(r));
+            }
+        }
+        // If not present, then we create it (APOC create.relationship code)
+        return Stream.of(new RelationshipResult(setProperties(from.createRelationshipTo(to,RelationshipType.withName(relType)),props)));
+    }
+
+    private boolean matchesProperties(Map<String, Object> allProperties, Map<String, Object> mergeProps) {
+        for (String key:mergeProps.keySet()){
+            if(!allProperties.get(key).equals(mergeProps.get(key))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private <T extends PropertyContainer> T setProperties(T pc, Map<String, Object> p) {
+        if (p == null) return pc;
+        for (Map.Entry<String, Object> entry : p.entrySet()) pc.setProperty(entry.getKey(), toPropertyValue(entry.getValue()));
+        return pc;
+    }
+
+    private Object toPropertyValue(Object value) {
+        if (value instanceof Iterable) {
+            Iterable it = (Iterable) value;
+            Object first = Iterables.firstOrNull(it);
+            if (first==null) return EMPTY_ARRAY;
+            return Iterables.asArray(first.getClass(), it);
+        }
+        return value;
     }
 
     public class RelationshipResult {
--- src/test/java/cspsp/merge/MergeRelationshipTest.java
+++ src/test/java/cspsp/merge/MergeRelationshipTest.java
@@ -24,7 +24,7 @@ public class MergeRelationshipTest {
             .withProcedure( MergeRelationship.class );
 
     @Test
-    public void shouldAllowIndexingAndFindingANode() throws Throwable
+    public void testRunOnce() throws Throwable
     {
         // In a try-block, to make sure we close the driver after the test
         try( Driver driver = GraphDatabase.driver( neo4j.boltURI() , Config.build().withEncryptionLevel( Config.EncryptionLevel.NONE ).toConfig() ) )
@@ -35,30 +35,41 @@ public class MergeRelationshipTest {
             Session session = driver.session();
 
             // And given I have a node in the database
-            long nodeId = session.run( " CREATE (p1:User {name:'Brookreson1'}) " +
+            String relType = session.run( " CREATE (p1:User {name:'Brookreson1'}) " +
                                        " CREATE (p2:User {name:'Brookreson2'}) " +
-                                       " WITH p1, p2 CALL cspsp.merge.relationship(p1, 'MYREL', '{a:1,b:\"valforb\"}', p2) YIELD rel " +
-                                       " RETURN id(rel) " )
+                                       " WITH p1, p2 CALL cspsp.merge.relationship(p1, 'MYREL', {a:1,b:'valforb',c:[3,4]}, p2) YIELD rel " +
+                                       " RETURN type(rel) " )
+                    .single()
+                    .get( 0 ).asString();
+
+            assert( relType.equals("MYREL"));
+        }
+    }
+
+
+    @Test
+    public void testRunMultipleTimes() throws Throwable
+    {
+        // In a try-block, to make sure we close the driver after the test
+        try( Driver driver = GraphDatabase.driver( neo4j.boltURI() , Config.build().withEncryptionLevel( Config.EncryptionLevel.NONE ).toConfig() ) )
+        {
+
+            // Given I've started Neo4j with the FullTextIndex procedure class
+            //       which my 'neo4j' rule above does.
+            Session session = driver.session();
+
+            // And given I have a node in the database
+            Long relCount = session.run( " CREATE (uno:Node)\n" +
+                    "CREATE (dos:Node)\n" +
+                    "WITH uno, dos\n" +
+                    "CALL cspsp.merge.relationship(uno, 'MYREL' ,{a:123, b:'4565'},  dos) YIELD rel AS r1\n" +
+                    "CALL cspsp.merge.relationship(uno, 'MYREL' ,{a:123, b:'4565'},  dos) YIELD rel AS r2\n" +
+                    "CALL cspsp.merge.relationship(uno, 'MYREL' ,{a:123, b:'4565'},  dos) YIELD rel AS r3\n" +
+                    "RETURN size((uno)-[:MYREL]-(dos)) AS size" )
                     .single()
                     .get( 0 ).asLong();
 
-            // When I use the index procedure to index a node
-            session.run( "CALL example.index({id}, ['name'])", parameters( "id", nodeId ) );
-
-            // Then I can search for that node with lucene query syntax
-            StatementResult result = session.run( "CALL example.search('User', 'name:Brook*')" );
-            //assertThat( result.single().get( "nodeId" ).asLong(), equalTo( nodeId ) );
+            assert( relCount.equals(new Long(1)));
         }
     }
 }
-
-
-/*
-CREATE (uno:Node)
-CREATE (dos:Node)
-WITH uno, dos
-CALL cspsp.merge.relationship(uno, 'MYREL' ,"{a:123, b:'4565'}",  dos) YIELD rel AS r1
-CALL cspsp.merge.relationship(uno, 'MYREL' ,"{a:123, b:'4565'}",  dos) YIELD rel AS r2
-CALL cspsp.merge.relationship(uno, 'MYREL' ,"{a:123, b:'4565'}",  dos) YIELD rel AS r3
-RETURN *
- */
